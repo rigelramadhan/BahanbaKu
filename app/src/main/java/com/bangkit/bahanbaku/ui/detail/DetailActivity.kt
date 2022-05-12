fa0f25578ebd518c17160140c91f98d49651851a
Rigel Ramadhan <rigelramadhan@gmail.com>
2022-05-12 16:40:49 +0700

    Progress of main layout and detail layout as well as needed item layouts.

---
+++ app/src/main/java/com/bangkit/bahanbaku/ui/detail/DetailActivity.kt
@@ -0,0 +1,17 @@
+package com.bangkit.bahanbaku.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.bahanbaku.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private val binding: ActivityDetailBinding by lazy {
+        ActivityDetailBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
