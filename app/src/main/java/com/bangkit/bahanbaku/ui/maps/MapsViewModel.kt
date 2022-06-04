a0cbbcd0a679c117d39f5312e520a0aff6eaa750
almedifa <srizalzal@gmail.com>
2022-06-04 21:01:57 +0700

    penambahan MapsViewModel.kt, SnapFoodViewModel.kt

--- app/src/main/java/com/bangkit/bahanbaku/ui/maps/MapsViewModel.kt
+++ app/src/main/java/com/bangkit/bahanbaku/ui/maps/MapsViewModel.kt
@@ -1,4 +1,19 @@
 package com.bangkit.bahanbaku.ui.maps
 
-class MapsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.bangkit.bahanbaku.data.repository.ProfileRepository
+import com.bangkit.bahanbaku.data.repository.RecipeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MapsViewModel@Inject constructor(
+    private val profileRepository: ProfileRepository
+) : ViewModel() {
+
+    fun getToken(): LiveData<String> {
+        return profileRepository.getToken().asLiveData()
+    }
 }
--- app/src/main/java/com/bangkit/bahanbaku/ui/snapfood/SnapFoodViewModel.kt
+++ app/src/main/java/com/bangkit/bahanbaku/ui/snapfood/SnapFoodViewModel.kt
@@ -1,14 +1,23 @@
 package com.bangkit.bahanbaku.ui.snapfood
 
 import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import com.bangkit.bahanbaku.data.repository.FoodRepository
+import com.bangkit.bahanbaku.data.repository.ProfileRepository
 import com.bangkit.bahanbaku.di.DatabaseModule
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SnapFoodViewModel @Inject constructor(private val foodRepository: FoodRepository) : ViewModel() {
+class SnapFoodViewModel @Inject constructor(
+    private val foodRepository: FoodRepository,
+    private val profileRepository: ProfileRepository
+    ) : ViewModel() {
 
+    fun getToken(): LiveData<String> {
+        return profileRepository.getToken().asLiveData()
+    }
 }
