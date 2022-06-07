7f3cbb1454f088fa982c0473daff27d162673142
Rigel Ramadhan <rigelramadhan@gmail.com>
2022-06-07 21:52:37 +0700

    Started to implement update location

--- app/src/main/java/com/bangkit/bahanbaku/ui/updatelocation/UpdateLocationActivity.kt
+++ app/src/main/java/com/bangkit/bahanbaku/ui/updatelocation/UpdateLocationActivity.kt
@@ -2,8 +2,10 @@ package com.bangkit.bahanbaku.ui.updatelocation
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.bangkit.bahanbaku.R
@@ -15,6 +17,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -41,28 +44,52 @@ class UpdateLocationActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         getMyLocation()
+    }
 
-//        fusedLocationClient.lastLocation.addOnSuccessListener {
-//
-//        }
+    private fun checkPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            getMyLocation()
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
+                getMyLocation()
+            }
+            permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
+                getMyLocation()
+            }
+            else -> {}
         }
     }
 
     private fun getMyLocation() {
-        if (ContextCompat.checkSelfPermission(
-                this.applicationContext,
-                Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED) {
-            mMap.isMyLocationEnabled = true
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) &&
+            checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    showUserPosition(location)
+                } else {
+                    Toast.makeText(this, getString(R.string.get_loc_failed), Toast.LENGTH_SHORT).show()
+                }
+            }
         } else {
-            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            requestPermissionLauncher.launch(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+            )
         }
+    }
+
+    private fun showUserPosition(location: Location) {
+        val currentLocation = LatLng(location.latitude, location.longitude)
+        mMap.addMarker(
+            MarkerOptions().position(currentLocation).title(getString(R.string.youre_here))
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 18f))
     }
 }
